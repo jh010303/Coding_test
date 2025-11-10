8a69ba56775e372b4641d04fc6e21745496118bb
JUNE <63304204+jh010303@users.noreply.github.com>
2025-11-10 12:19:39 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -7,29 +7,24 @@ using namespace std;
 unordered_map<string, int> kind_index;
 vector<vector<string>> clothes(31,vector<string>());
 
-void init_clothes() {
-	for (int i = 0; i < 31; i++) {
-		clothes[i].clear();
-	}
-}
-
 int main() {
-	int t, n,ans=1;
+	int t, n,ans=1,cloth_i;
 	string cloth, kind;
 	cin >> t;
 	for (int i = 0; i < t; i++) {
 		cin >> n;
 		ans = 1;
 		kind_index.clear();
-		init_clothes();
+		clothes = vector<vector<string>>(31, vector <string>());
 		for (int j = 0; j < n; j++) {
 			cin >> cloth >> kind;
-			if (kind_index[kind] == 0) {
+			cloth_i = kind_index[kind];
+			if (cloth_i == 0) {
 				kind_index[kind] = j+1;
 				clothes[j].push_back(cloth);
 			}
 			else {
-				clothes[kind_index[kind] - 1].push_back(cloth);
+				clothes[cloth_i - 1].push_back(cloth);
 			}
 		}
 		for (auto kind : kind_index) {
