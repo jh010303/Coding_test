e6ba069e5bc66d6a48f41804c9e45e3a2ec405f8
JUNE <63304204+jh010303@users.noreply.github.com>
2025-11-22 11:44:26 +0900

    [D2] Title: 두 개의 숫자열, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <memory.h>
 
 using namespace std;
 
-vector<int> list_a;
-vector<int> list_b;
+int ai[21];
+int bi[21];
+int main(int argc, char** argv)
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 
-int main() {
-	int t, n, m, max_ele = -1, temp, sum = 0,index=0;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		list_a.clear();
-		list_b.clear();
-		max_ele = -1;
+	int test_case,T,ans=-1000001,sum=0,origin_index=0;
+	int n, m;
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
 		cin >> n >> m;
-		for (int j = 0; j < n; j++) {
-			cin >> temp;
-			list_a.push_back(temp);
+		memset(ai, 0, sizeof(ai));
+		memset(bi, 0, sizeof(bi));
+		ans = -1000001;
+		sum = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> ai[i];
 		}
-		for (int j = 0; j < m; j++) {
-			cin >> temp;
-			list_b.push_back(temp);
+		for (int i = 0; i < m; i++) {
+			cin >> bi[i];
 		}
-		if (n < m) {
-			for (int j = 0; j <= m-n; j++) {
+
+		// ai 기준
+		if (n >= m) {
+			for (int i = 0; i <= n - m; i++) {
 				sum = 0;
-				index = 0;
-				for (int r = j; r < j+n; r++) {
-					sum += list_a[index++] * list_b[r];
+				origin_index = 0;
+				for (int j = i; j <= i + m; j++) {
+					sum += (ai[j] * bi[origin_index++]);
 				}
-				max_ele = max(sum, max_ele);
+				ans = max(ans, sum);
 			}
 		}
+		// bi 기준
 		else {
-			for (int j = 0; j <= n-m; j++) {
+			for (int i = 0; i <= m - n; i++) {
 				sum = 0;
-				index = 0;
-				for (int r = j; r < j + m; r++) {
-					sum += list_a[r] * list_b[index++];
+				origin_index = 0;
+				for (int j = i; j <= i + n; j++) {
+					sum += (ai[origin_index++] * bi[j]);
 				}
-				max_ele = max(sum, max_ele);
+				ans = max(ans, sum);
 			}
 		}
-		cout << '#' << i + 1 << ' ' << max_ele << '\n';
+		cout << '#' << test_case << ' ' << ans << '\n';
 	}
+	return 0;
 }
