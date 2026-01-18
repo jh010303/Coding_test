c055dc72bb76b2220bb7b618478b650a755ebfc4
JUNE <63304204+jh010303@users.noreply.github.com>
2026-01-18 23:31:47 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 133 ms, Memory: 35,144 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution
+{
+	static int n,y,x;
+	static String s,cmd;
+	static ArrayList<String> cryptogram = new ArrayList<>();
+	
+	public static void main(String args[]) throws Exception
+	{	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		for(int test_case=1; test_case<=10; test_case++) {
+			cryptogram.clear(); 
+			n = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) {
+				cryptogram.add(st.nextToken());
+			}
+			n = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<n; i++) {
+				cmd = st.nextToken();
+				if(cmd.equals("I")) {
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					for (int j = x; j < x + y; j++) {
+						cryptogram.add(j, st.nextToken());
+					}
+				}else if(cmd.equals("D")) {
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					for(int j=x; j<x+y; j++) {
+						cryptogram.remove(j);
+					}
+				}else if(cmd.equals("A")) {
+					y = Integer.parseInt(st.nextToken());
+					for(int j=0; j<y; j++) {
+						cryptogram.add(st.nextToken());
+					}
+				}
+			}
+			
+			System.out.print("#"+test_case+" ");
+			for(int i=0; i<cryptogram.size(); i++) {
+				if(i<10) {
+					System.out.print(cryptogram.get(i)+" ");
+				}
+				else break;
+			}
+			System.out.println();
+		}
+	}
+}
