8d4a5789121a9c55e3d3574df01ddf8c22529bac
JUNE <63304204+jh010303@users.noreply.github.com>
2026-01-25 15:09:28 +0900

    [D4] Title: [Professional] 키 순서, Time: 2,714 ms, Memory: 97,040 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -10,96 +10,62 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int t,n,m,ans;
-	static ArrayList<ArrayList<Integer>> bigger = new ArrayList<ArrayList<Integer>>();
-	static ArrayList<ArrayList<Integer>> shorter = new ArrayList<ArrayList<Integer>>();
-	static boolean biggerVisited[] = new boolean[501];
-	static boolean shorterVisited[] = new boolean[501];
-	static Set<Integer> wholeStudents = new HashSet<>();
-	
+	static boolean[][] graph = new boolean[501][501];
+	static int ans, t;
+
 	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		t = Integer.parseInt(br.readLine());
-		for(int test_case=1; test_case<=t; test_case++) {
-			n = Integer.parseInt(br.readLine());
-			m = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= t; test_case++) {
 			init();
-			for(int i=0; i<m; i++) {
+			int n = Integer.parseInt(br.readLine());
+			int m = Integer.parseInt(br.readLine());
+			for (int i = 0; i < m; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				bigger.get(a).add(b);
-				shorter.get(b).add(a);
+				graph[a][b] = true;
 			}
-			
-			for(int i=1; i<=n; i++) {
-				biggerVisited[i] = true;
-				biggerDfs(i,i);
-				initVisited();
-				shorterVisited[i] = true;
-				shorterDfs(i,i);
-				initVisited();
+			for (int i = 1; i <= n; i++) {
+				graph[i][i] = true;
 			}
-			
-			for(int i=1; i<=n; i++) {
-				wholeStudents.add(i);
-				for(Integer v: bigger.get(i)) {
-					wholeStudents.add(v);
+			for (int k = 1; k <= n; k++) {
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= n; j++) {
+						graph[i][j] = graph[i][j] || (graph[i][k] && graph[k][j]);
+					}
 				}
-				for(Integer v: shorter.get(i)) {
-					wholeStudents.add(v);
+			}
+			for (int i = 1; i <= n; i++) {
+				int cnt = 0;
+				for (int j = 1; j <= n; j++) {
+					if (graph[i][j]) {
+						cnt++;
+					}
+					if (graph[j][i]) {
+						cnt++;
+					}
 				}
-				if(wholeStudents.size()==n) {
+
+				if (cnt == n + 1) {
 					ans++;
 				}
-				wholeStudents.clear();
+
 			}
 			bw.write("#"+test_case+" "+ans);
 			bw.newLine();
 			bw.flush();
 		}
 	}
-	
-	static void biggerDfs(int start, int v) {
-		int size = bigger.get(v).size();
-		for(int i=0; i<size; i++) {
-			int nextV = bigger.get(v).get(i);
-			if(!biggerVisited[nextV]) {
-				biggerVisited[nextV] = true;
-				biggerDfs(start, nextV);
-				bigger.get(start).add(nextV);
-			}
-		}
-	}
-	
-	static void shorterDfs(int start, int v) {
-		int size = shorter.get(v).size();
-		for(int i=0; i<size; i++) {
-			int nextV = shorter.get(v).get(i);
-			if(!shorterVisited[nextV]) {
-				shorterVisited[nextV] = true;
-				shorterDfs(start, nextV);
-				shorter.get(start).add(nextV);
-			}
-		}
-	}
-	
+
 	static void init() {
-		bigger.clear();
-		shorter.clear();
-		for(int i=0; i<=n; i++) {
-			bigger.add(new ArrayList<Integer>());
-			shorter.add(new ArrayList<Integer>());
-		}
-		initVisited();
-		ans=0;
-	}
-	
-	static void initVisited() {
-		for(int i=0; i<501; i++) {
-			Arrays.fill(shorterVisited, false);
-			Arrays.fill(biggerVisited, false);
+		ans = 0;
+		for (int i = 0; i < 501; i++) {
+			for (int j = 0; j < 501; j++) {
+				Arrays.fill(graph[i], false);
+			}
 		}
 	}
 
