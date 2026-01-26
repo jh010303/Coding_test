a3ff09f1e43350247417decd8256d096214cb561
JUNE <63304204+jh010303@users.noreply.github.com>
2026-01-26 23:31:52 +0900

    [Silver II] Title: 참외밭, Time: 104 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -1,51 +1,46 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int k = Integer.parseInt(br.readLine());
-        
-        int[][] shape = new int[6][2];
-        int maxW = 0, maxH = 0;
-        int wIdx = -1, hIdx = -1; 
+	static int k, widthMax = Integer.MIN_VALUE, heightMax = Integer.MIN_VALUE
+			,widthMaxIndex=0, heightMaxIndex=0;
+	static int[][] shape = new int[6][2];
 
-        for (int i = 0; i < 6; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            shape[i][0] = Integer.parseInt(st.nextToken());
-            shape[i][1] = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		k = Integer.parseInt(br.readLine());
 
-            if (shape[i][0] == 1 || shape[i][0] == 2) {
-                if (shape[i][1] > maxW) {
-                    maxW = shape[i][1];
-                    wIdx = i;
-                }
-            } else {
-                if (shape[i][1] > maxH) {
-                    maxH = shape[i][1];
-                    hIdx = i;
-                }
-            }
-        }
+		for (int i = 0; i < 6; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int dir = Integer.parseInt(st.nextToken());
+			int length = Integer.parseInt(st.nextToken());
+			shape[i][0] = dir;
+			shape[i][1] = length;
+		}
 
-
-        int smallW = Math.abs(shape[(wIdx + 5) % 6][1] - shape[(wIdx + 1) % 6][1]);
-        int smallH = Math.abs(shape[(hIdx + 5) % 6][1] - shape[(hIdx + 1) % 6][1]);
-
-        int bigArea = maxW * maxH;
-        int smallArea = smallW * smallH;
-        int ans = (bigArea - smallArea) * k;
-
-        bw.write(String.valueOf(ans));
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		for(int i=0; i<6; i++) {
+			if((shape[i][0] == 1 || shape[i][0]==2) && shape[i][1]>widthMax) {
+				widthMax = shape[i][1];
+				widthMaxIndex = i;
+			}
+			else if((shape[i][0] == 3 || shape[i][0]==4) && shape[i][1]>heightMax) {
+				heightMax = shape[i][1];
+				heightMaxIndex = i;
+			}
+		}
+		int ans = heightMax*widthMax - 
+				(Math.abs(shape[(heightMaxIndex+1)%6][1] - shape[(heightMaxIndex+5)%6][1]))*
+				(Math.abs(shape[(widthMaxIndex+1)%6][1] - shape[(widthMaxIndex+5)%6][1]));
+		bw.write(ans * k + "");
+		bw.flush();
+	}
 }
