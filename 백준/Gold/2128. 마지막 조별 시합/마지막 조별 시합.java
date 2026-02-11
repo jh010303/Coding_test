e726a79394f60a6af6cb374a82fe87453abe6241
JUNE <63304204+jh010303@users.noreply.github.com>
2026-02-11 23:14:02 +0900

    [Gold IV] Title: 마지막 조별 시합, Time: 116 ms, Memory: 12592 KB -BaekjoonHub

--- 백준/Gold/2128. 마지막 조별 시합/마지막 조별 시합.java
+++ 백준/Gold/2128. 마지막 조별 시합/마지막 조별 시합.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int n,d,k,ans;
-    static List<List<Integer>> students = new ArrayList<List<Integer>>();
+    static List<Integer> students = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
@@ -11,26 +11,25 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        init();
 
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            int index = Integer.parseInt(st.nextToken());
-            for(int j=0; j<index; j++) {
-                students.get(i).add(Integer.parseInt(st.nextToken()));
+            int cnt = Integer.parseInt(st.nextToken());
+            int studentBit = 0;
+            for(int j=0; j<cnt; j++) {
+            	int bitOn = Integer.parseInt(st.nextToken());
+            	studentBit |= (1<<(bitOn-1));
             }
+            students.add(studentBit);
         }
 
-        int temp = 0,count=0;
+        int count=0;
         for(int i=1; i<(1<<d); i++) {
             if(countOnBit(i)==k) {
                 count=0;
                 for(int j=0; j<n; j++) {
-                    temp = 0;
-                    for(int k=0; k<students.get(j).size(); k++) {
-                        temp+=(1 << (students.get(j).get(k)-1));
-                    }
-                    if((i&temp)==temp) {
+                	int studentsBit = students.get(j);
+                    if((i&studentsBit)==studentsBit) {
                         count++;
                     }
                 }
@@ -50,12 +49,5 @@ public class Main {
             n/=2;
         }
         return count;
-    }
-
-    static void init() {
-        students.clear();
-        for(int i=0; i<n; i++) {
-            students.add(new ArrayList<>());
-        }
     }
 }
