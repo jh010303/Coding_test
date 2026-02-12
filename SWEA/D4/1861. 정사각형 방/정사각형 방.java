a530ca2aa9759fe95b4141e369528208f6454790
JUNE <63304204+jh010303@users.noreply.github.com>
2026-02-12 16:51:02 +0900

    [D4] Title: 정사각형 방, Time: 130 ms, Memory: 35,072 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.java
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -2,10 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static class Room{
+	static class Room {
 		int y;
 		int x;
 		int moveCnt;
+
 		public Room(int y, int x, int moveCnt) {
 			super();
 			this.y = y;
@@ -13,14 +14,13 @@ public class Solution {
 			this.moveCnt = moveCnt;
 		}
 	}
-	
-	static int roomAns, cntAns;
+
+	static int roomAns, cntAns, n;
 	static int[][] rooms;
-	static int[][] dp;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
-	static Queue<Room> que = new ArrayDeque<>();
-	
+	static int[][] cache;
+	static int[] dx = { 1, -1, 0, 0 };
+	static int[] dy = { 0, 0, 1, -1 };
+
 	public static void main(String args[]) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -30,55 +30,52 @@ public class Solution {
 		for (int test_case = 1; test_case <= TC; test_case++) {
 			init();
 			sb.append("#").append(test_case).append(" ");
-			int n = Integer.parseInt(in.readLine());
+			n = Integer.parseInt(in.readLine());
 			rooms = new int[n][n];
-			dp = new int[n][n];
-			
-			for(int i=0; i<n; i++) {
+			cache = new int[n][n];
+
+			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(in.readLine());
-				for(int j=0; j<n; j++) {
+				for (int j = 0; j < n; j++) {
 					rooms[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-	
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(dp[i][j]>0) continue;
-					que.offer(new Room(i,j,dp[i][j]));
-					int start = rooms[i][j];
-					while(!que.isEmpty()) {
-						Room curRoom = que.poll();
-						int curY= curRoom.y, curX = curRoom.x, curCnt = curRoom.moveCnt, curNum = rooms[curY][curX];
-						for(int r=0; r<4; r++) {
-							int nextY = curY+dy[r], nextX = curX+dx[r], nextCnt=curCnt+1;
-							if(nextY<0 || nextX<0 || nextX>=n || nextY>=n) continue;
-							int nextNum = rooms[nextY][nextX];
-							if(nextNum == curNum+1) {
-								dp[nextY][nextX] = nextCnt;
-								que.offer(new Room(nextY,nextX,nextCnt));
-								if(cntAns<nextCnt) {
-									cntAns = nextCnt;
-									roomAns = start;
-								}
-								else if(cntAns==nextCnt) {
-									if(roomAns>start) {
-										roomAns = start;
-									}									
-								}
-							}
-						}
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (cache[i][j] > 0)
+						continue;
+					int cnt = backTracking(i, j);
+					if (cnt > cntAns) {
+						cntAns = cnt;
+						roomAns = rooms[i][j];
+					} else if (cnt == cntAns) {
+						roomAns = Math.min(roomAns, rooms[i][j]);
 					}
 				}
 			}
-			
-			sb.append(roomAns).append(" ").append(cntAns+1).append("\n");
+
+			sb.append(roomAns).append(" ").append(cntAns + 1).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-	
+
+	static int backTracking(int y, int x) {
+		for (int r = 0; r < 4; r++) {
+			int nextY = y + dy[r], nextX = x + dx[r];
+			if (nextY < 0 || nextX < 0 || nextX >= n || nextY >= n)
+				continue;
+			int curRoomNum = rooms[y][x], nextRoomNum = rooms[nextY][nextX];
+			if (nextRoomNum == curRoomNum + 1) {
+				cache[y][x] = backTracking(nextY, nextX) + 1;
+				break;
+			}
+		}
+		return cache[y][x];
+	}
+
 	static void init() {
 		roomAns = Integer.MAX_VALUE;
 		cntAns = 0;
 	}
 }
-
