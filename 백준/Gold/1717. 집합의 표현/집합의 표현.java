fbdea2a7ce983ab16b09b16889da50164bd7f1a2
JUNE <63304204+jh010303@users.noreply.github.com>
2026-03-14 16:34:23 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 48196 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -11,18 +11,17 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		parent = new int[n+1];
-		for(int i=0; i<=n; i++) {
-			parent[i] = i;
-		}
+		Arrays.fill(parent, -1);
+		
 		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int cmd = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
+			
 			if(cmd==0) {
 				union(a,b);
-			}
-			else if(cmd==1) {
+			}else if(cmd==1) {
 				if(find(a)==find(b)) {
 					sb.append("YES").append("\n");
 				}
@@ -31,18 +30,27 @@ public class Main {
 				}
 			}
 		}
+		
 		System.out.print(sb);
+	}
+	
+	static int find(int n) {
+		if(parent[n]<0) return n;
+		parent[n] = find(parent[n]);
+		return parent[n];
+		
 	}
 	
 	static void union(int a, int b) {
 		int parentA = find(a); int parentB = find(b);
 		if(parentA==parentB) return;
-		parent[parentB] = parentA;
-	}
-	
-	static int find(int n) {
-		if(parent[n]==n) return n;
-		parent[n] = find(parent[n]);
-		return parent[n];
+		else if(parent[parentA]>parent[parentB]) {
+			parent[parentB]+=parent[parentA];
+			parent[parentA]=parentB;
+		}
+		else {
+			parent[parentA]+=parent[parentB];
+			parent[parentB]=parentA;
+		}
 	}
 }
