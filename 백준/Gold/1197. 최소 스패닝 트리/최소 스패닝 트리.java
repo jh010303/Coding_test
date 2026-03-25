2a66c870870d8364046cf9a5b62f2b0e1b47bb67
JUNE <63304204+jh010303@users.noreply.github.com>
2026-03-25 10:34:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 480 ms, Memory: 58004 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,74 +1,82 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		int s;
-		int n;
-		int w;
-		public Edge(int s, int n, int w) {
-			this.s = s;
-			this.n = n;
-			this.w = w;
-		}
+	static int V;//정점
+	static int total;
+	static boolean[] visited;
+	static int E;//간선 
+	static List<Node>[] list;
+	static StringBuilder sb = new StringBuilder();
+
+	static class Node implements Comparable<Node>{
+		int b; //연결된 정점
+		int c; //가중치
 		
-		@Override
-		public int compareTo(Edge e) {
-			return this.w - e.w;
-		}
-	}
-	
-	static int v,e,ans;
-	static Edge[] tree;
-	static int[] parent;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		tree = new Edge[e];
-		parent = new int[v+1];
-		Arrays.fill(parent, -1);
-		
-		for(int i=0; i<e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			tree[i] = new Edge(s,n,w);
+		Node(int b, int c){
+			this.b = b;
+			this.c = c;
 		}
 
-		Arrays.sort(tree);
+		@Override
+		public int compareTo(Node o) {
+			return this.c-o.c;
+		}
 		
-		for(int i=0; i<e; i++) {
-			Edge cur = tree[i];
-			int s = cur.s; int n = cur.n; int w = cur.w;
-			if(find(s)!=find(n)) {
-				union(s,n);
-				ans+=w;
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		list = new ArrayList[V+1];
+		visited = new boolean[V+1];
+		
+		for(int i=1;i<=V;i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i=0;i<E;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			list[a].add(new Node(b,c));
+			list[b].add(new Node(a,c));
+		}
+		select(1);
+		System.out.println(total);
+
+	}
+
+	public static void select(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(start, 0));
+		total = 0;
+		
+		while(!pq.isEmpty()) {
+			Node tmp = pq.poll();
+			int edge = tmp.b;
+			int cost = tmp.c;
+			
+			if(!visited[edge]) {
+				visited[edge] = true;
+				
+				total += cost;
+				for(Node l : list[edge]) {
+					if(!visited[l.b]) {
+						pq.offer(new Node(l.b,l.c));
+					}
+				}
 			}
 		}
-		
-		
-		System.out.print(ans);
+
 	}
-	
-	static int find(int n) {
-		if(parent[n]<0) return n;
-		parent[n] = find(parent[n]);
-		return parent[n];
-	}
-	
-	static void union(int a, int b) {
-		int parentA = find(a); int parentB = find(b);
-		if(parentA==parentB)return;
-		else if(parentA<parentB) {
-			parent[parentA]+=parent[parentB];
-			parent[parentB] = parentA;
-		}
-		else {
-			parent[parentB]+=parent[parentA];
-			parent[parentA] = parentB;
-		}
-	}
+
 }
