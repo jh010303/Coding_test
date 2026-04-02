8905b471cedc1c53779e5f2aa333de16b84cf2b1
JUNE <63304204+jh010303@users.noreply.github.com>
2026-04-02 12:05:24 +0900

    [Gold IV] Title: 인구 이동, Time: 640 ms, Memory: 299324 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,100 +1,78 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-	static class Cord{
-		int y;
-		int x;
-		public Cord(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-	
-	static int n,l,r;
-	static int[][] map;
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
-	static boolean[][] visited;
-	static Queue<Cord> que = new LinkedList<>();
-	static Queue<Cord> unionQ = new LinkedList<>();
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		l = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
-		map = new int[n][n];
-		visited = new boolean[n][n];
-		
-		for(int i=0; i<n; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int[] dx = {0,0,-1,1}, dy = {-1,1,0,0};
+		int[][] board = new int[N][N];
+		for(int i = 0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<n; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for(int j = 0; j<N; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		int day = 0;
-		boolean move = false;
-		
-		do {
-			move = false;
-			init();
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(!visited[i][j]) {
-						visited[i][j] = true;
-						Cord temp = new Cord(i,j);
-						que.offer(temp);
-						unionQ.offer(temp);
-						int sum = map[i][j]; int cnt = 1;
-						while(!que.isEmpty()) {
-							Cord cur = que.poll();
-							int cy = cur.y; int cx = cur.x; int cw = map[cy][cx];
-							for(int t=0; t<4; t++) {
-								int ny = cy+dy[t]; int nx = cx+dx[t];
-								if(ny<0 || nx<0 || ny>=n || nx>=n || visited[ny][nx]) {
-									continue;
-								}
-								int nw = map[ny][nx];
-								if(l<=Math.abs(cw-nw) && Math.abs(cw-nw)<=r) {
-									visited[ny][nx] = true;
-									Cord next = new Cord(ny,nx);
-									que.offer(next);
-									unionQ.offer(next);
-									sum+=map[ny][nx];
-									cnt++;
-								}
-							}
-						}
-						if(cnt==1) { // 움직이지 않았다는 것
-							unionQ.clear();
-						}
-						else {	// 움직였다는 것		
-							
-							int newP = sum/cnt;
-							move = true;
-							while(!unionQ.isEmpty()) {
-								Cord cur = unionQ.poll();
-								map[cur.y][cur.x] = newP;
+		int ans = 0;
+		while(true) {
+			int[][] vis = new int[N][N];
+			boolean sumed = false;
+			for(int i = 0; i<N; i++) {
+				for(int j = 0; j<N; j++) {
+					if(vis[i][j] != 0) continue;
+					List<Integer> area = new ArrayList<>();
+					Deque<Integer> Q = new ArrayDeque<>();
+					Q.offer(i * N + j);
+					int temp1 = i * N + j;
+					vis[i][j] = 1;
+					while(!Q.isEmpty()) {
+						int temp = Q.poll();
+						int row = temp / N;
+						int col = temp % N;
+						for(int q = 0; q<4; q++) {
+							int nrow = row + dx[q];
+							int ncol = col + dy[q];
+							if(nrow >= N || nrow < 0 || ncol < 0 || ncol >= N) continue;
+							if(vis[nrow][ncol] != 0) continue;
+							int sub = Math.abs(board[row][col] - board[nrow][ncol]);
+							if(sub >= L && sub <= R) {
+								Q.offer(nrow * N + ncol);
+								area.add(nrow * N + ncol);
+								vis[nrow][ncol] = 1;
 							}
 						}
 					}
+					int temp3 = board[temp1 / N][temp1 % N];
+					for(int q = 0; q<area.size(); q++) {
+						int temp2 = area.get(q);
+						temp3 += board[temp2 / N][temp2 % N];
+					}
+					for(int q = 0; q<area.size(); q++) {
+						int temp2 = area.get(q);
+						board[temp2 / N][temp2 % N] = temp3 / (area.size() + 1);
+
+						board[temp1 / N][temp1 % N] = temp3 / (area.size() + 1);
+						sumed = true;
+					}
 				}
 			}
-			if(move) {
-				day++;
-			}
+//			for(int z = 0; z<N; z++) {
+//				for(int x = 0; x < N; x++) {
+//					System.out.print(board[z][x] + "\t");
+//				}
+//				System.out.println("");
+//			}
+//			System.out.println("");
+//			
+			if(!sumed) break;
+			else ans++;
 		}
-		while(move);
-
-		System.out.print(day);
+		
+		
+		System.out.println(ans);
+		
+		
 	}
-	
-	static void init() {
-		for(int i=0; i<n; i++) {
-			Arrays.fill(visited[i], false);
-		}
-	}
-	
 }
