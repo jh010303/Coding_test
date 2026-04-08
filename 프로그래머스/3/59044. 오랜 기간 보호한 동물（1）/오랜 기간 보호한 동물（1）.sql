89330c1855ee489f80e18d6587796fd2eef5c206
JUNE <63304204+jh010303@users.noreply.github.com>
2026-04-08 16:23:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,17 @@
+-- 코드를 입력하세요
+SELECT NAME, DATETIME
+    FROM ANIMAL_INS 
+    WHERE ANIMAL_ID NOT IN 
+    (
+        SELECT ANIMAL_ID
+            FROM ANIMAL_OUTS
+    )
+    ORDER BY DATETIME
+    LIMIT 3
+    
+
+# WHERE
+# GROUP BY
+# HAVING
+# ORDER BY
+# LIMIT
