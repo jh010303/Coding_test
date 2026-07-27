ddb5642ff89411efe4ec4a74ec546c96515f8edb
JUNE <63304204+jh010303@users.noreply.github.com>
2026-07-27 22:15:00 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 170 ms, Memory: 35,488 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
@@ -1,58 +1,59 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Solution {
-	static int ans;
-	static char[][] map = new char[4][4];
-	static Set<String> set = new HashSet<>();
-	static int[] dx = {1,-1,0,0};
-	static int[] dy = {0,0,1,-1};
-	public static void main(String args[]) throws Exception {
+
+class Solution
+{
+    static int[][] map = new int[4][4];
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,1,-1};
+    
+    static Set<List<Integer>> set = new HashSet<>();
+    
+	public static void main(String args[]) throws Exception
+	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		for (int test_case = 1; test_case <= T; test_case++) {
-			init();
-			for(int i=0; i<4; i++) {
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            set.clear();
+			for(int i=0; i<4; i++){
 				st = new StringTokenizer(br.readLine());
-				for(int j=0; j<4; j++) {
-					map[i][j] = st.nextToken().charAt(0);
-				}
-			}
-			StringBuilder num = new StringBuilder();
-			for(int i=0; i<4; i++) {
-				for(int j=0; j<4; j++) {					
-					backTracking(i,j,num,0);
-				}
-			}
-			
-			ans = set.size();
-			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
-		}
+                for(int j=0; j<4; j++){
+                	map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
 		
-		System.out.print(sb);
-	}
-	
-	static void backTracking(int y, int x, StringBuilder num, int depth) {
-		if(depth==7) {
-			set.add(num.toString());
-			return;
+			List<Integer> number = new ArrayList<>();
+            for(int i=0; i<4; i++){
+            	for(int j=0; j<4; j++){
+                	dfs(i,j,0,number);
+                }
+            }
+			
+            sb.append("#").append(test_case).append(" ").append(set.size()).append("\n");
 		}
-		for(int i=0; i<4; i++) {
-			int nextY = y+dy[i]; int nextX = x+dx[i];
-			if(nextY<0 || nextX<0 || nextY>=4 || nextX>=4) {
-				continue;
-			}
-			num.append(map[nextY][nextX]);
-			backTracking(nextY,nextX,num,depth+1);
-			num.deleteCharAt(num.length()-1);
-		}
+        System.out.print(sb);
 	}
-	
-	static void init() {
-		ans = 0;
-		map = new char[4][4];
-		set.clear();
-	}
+    
+    static void dfs(int y, int x, int cnt, List<Integer> number){
+    	if(cnt==7) {
+    		set.add(number);
+    		return;
+    	}
+    	
+    	number.add(map[y][x]);
+    	for(int i=0; i<4; i++) {
+    		int ny = y+dy[i];
+    		int nx = x+dx[i];
+            if(ny<0 || nx<0 || ny>=4 || nx>=4){
+            	continue;
+            }
+    		dfs(ny,nx,cnt+1,number);
+    	}
+    	number.remove(cnt);
+    }
 }
