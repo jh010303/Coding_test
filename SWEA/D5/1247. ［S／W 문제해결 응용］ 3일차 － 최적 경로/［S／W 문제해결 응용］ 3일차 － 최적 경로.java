9968886c90aad2819dd0c3b95127167471b6449e
JUNE <63304204+jh010303@users.noreply.github.com>
2026-07-29 17:14:29 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 901 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,65 +1,78 @@
 import java.util.*;
 import java.io.*;
 
-public class Solution {
-	static class Cord{
-		int x;
-		int y;
-		public Cord(int x, int y) {
-			this.x = x;
-			this.y = y;
+class Solution
+{
+    static class Cord{
+    	int y;
+        int x;
+        public Cord(int y, int x){
+        	this.y = y;
+            this.x = x;
+        }
+    }
+    
+    static boolean[] visited;
+    static Cord[] cords;
+    static int answer = Integer.MAX_VALUE;
+    
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int n = Integer.parseInt(br.readLine());
+            cords = new Cord[n];
+            visited = new boolean[n];
+            answer = Integer.MAX_VALUE;
+            
+            st = new StringTokenizer(br.readLine());
+            int sy = Integer.parseInt(st.nextToken());
+            int sx = Integer.parseInt(st.nextToken());
+            int ey = Integer.parseInt(st.nextToken());
+            int ex = Integer.parseInt(st.nextToken());
+            
+            for(int i=0; i<n; i++){
+                int y = Integer.parseInt(st.nextToken());
+                int x = Integer.parseInt(st.nextToken());
+            	cords[i] = new Cord(y,x);
+            }
+            
+            for(int i=0; i<n; i++){
+                visited[i] = true;
+                int cy = cords[i].y; int cx = cords[i].x;
+            	backTracking(Math.abs(sy-cy)+Math.abs(sx-cx),1,n,cy,cx,ey,ex);
+                visited[i] = false;
+            }
+            
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
+        System.out.print(sb);
 	}
-	static Cord[] customers;
-	static Cord company = new Cord(0,0);
-	static Cord house = new Cord(0,0);
-	static boolean[] visited;
-	static int n,answer;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int t = Integer.parseInt(br.readLine());
-		for(int test_case=1; test_case<=t; test_case++) {
-			n = Integer.parseInt(br.readLine());
-			answer=Integer.MAX_VALUE;
-			customers = new Cord[n];
-			visited = new boolean[n];
-			st = new StringTokenizer(br.readLine());
-			company.x = Integer.parseInt(st.nextToken());
-			company.y = Integer.parseInt(st.nextToken());
-			house.x = Integer.parseInt(st.nextToken());
-			house.y = Integer.parseInt(st.nextToken());
-			
-			for(int i=0; i<n; i++) {
-				customers[i] = new Cord(0,0);
-				customers[i].x = Integer.parseInt(st.nextToken());
-				customers[i].y = Integer.parseInt(st.nextToken());
-			}
-			
-			
-			backTracking(company, 0,0);
-			sb.append("#").append(test_case).append(" ").append(answer).append("\n");
-		}
-		System.out.print(sb);
-	}
-	
-	static void backTracking(Cord cur, int distance, int cnt) {
-		int curY = cur.y; int curX = cur.x;
-		if(cnt==n) {
-			distance+=(Math.abs(house.y-curY)+Math.abs(house.x-curX));
-			answer = Math.min(answer,distance);
-			return;
-		}
-		for(int i=0; i<n; i++) {
-			if(!visited[i]) {
-				visited[i]=true;
-				Cord next = customers[i];
-				backTracking(next,distance+Math.abs(next.y-curY)+Math.abs(next.x-curX),cnt+1);
-				visited[i]=false;
-			}
-		}
-	}
+    
+    static void backTracking(int len, int cnt, int n, int cy, int cx, int ey, int ex){
+        if(cnt==n){
+            len+=(Math.abs(ey-cy)+Math.abs(ex-cx));
+            answer = Math.min(answer,len);
+            return;
+        }
+        
+        for(int i=0; i<n; i++){
+        	if(!visited[i]){
+            	visited[i] = true;
+                int y = cords[i].y; int x = cords[i].x;
+                backTracking(len+Math.abs(cy-y)+Math.abs(cx-x), cnt+1,n,y,x,ey,ex);
+                visited[i] = false;
+            }
+        }
+        
+        
+    }
+    
+    static int getD(int y2, int x2, int y1, int x1){
+    	return Math.abs(y2-y1)+Math.abs(x2-x1);
+    }
 }
