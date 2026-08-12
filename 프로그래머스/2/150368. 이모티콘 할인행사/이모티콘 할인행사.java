8b8a66aaa586fc5d547b1528c18663f766367c3d
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-12 16:52:47 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 59.58 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,79 +1,72 @@
 import java.util.*;
 
 class Solution {
-    class Emoticon{
-        int d;
-        int p;
-        public Emoticon(int d, int p){
-            this.d = d;
-            this.p = p;
-        }
-    }
-    
-    class Sell implements Comparable<Sell>{
+    class TotalEmoticon{
         int cnt;
-        int p;
+        int price;
         
-        public Sell(int cnt, int p){
+        public TotalEmoticon(int cnt, int price){
             this.cnt = cnt;
-            this.p = p;
+            this.price = price;
         }
-        
-        @Override
-        public int compareTo(Sell s){
-            if(this.cnt!=s.cnt){
-                return Integer.compare(s.cnt,this.cnt);
-            }
-            return Integer.compare(s.p,this.p);
-        }
-        
     }
     
-    Emoticon[] emoticonList;
-    int[] discounts = {40,30,20,10};
+    PriorityQueue<TotalEmoticon> cntPq = new PriorityQueue<>((a,b)->{
+        return b.cnt-a.cnt;
+    });
     
-    PriorityQueue<Sell> sellQ = new PriorityQueue<>();
+    PriorityQueue<TotalEmoticon> pricePq = new PriorityQueue<>((a,b)->{
+        return b.price-a.price;
+    });
+    
+    int[] discounts = {10,20,30,40};
+    int cntMax = 0;
     
     public int[] solution(int[][] users, int[] emoticons) {
         int[] answer = new int[2];
-        emoticonList = new Emoticon[emoticons.length];
-        combine(users,emoticons,0);
-
-        Sell s = sellQ.poll();
-        answer[0] = s.cnt;
-        answer[1] = s.p;
+        
+        dfs(0,new double[emoticons.length], users, emoticons);
+        
+        while(!cntPq.isEmpty()){
+            TotalEmoticon temp = cntPq.poll();
+            if(cntMax>temp.cnt){
+                break;
+            }
+            pricePq.offer(temp);
+        }
+        
+        TotalEmoticon temp = pricePq.poll();
+        answer[0] = temp.cnt; answer[1] = temp.price;
         return answer;
     }
     
-    void combine(int[][] users, int[] emoticons, int index){
-        if(index>=emoticons.length){ 
-            doSell(users);
-            return;
-        }
-        for(int i=0; i<4; i++){
-            emoticonList[index] = new Emoticon(discounts[i],emoticons[index]);
-            combine(users,emoticons,index+1);
-        }
-    }
-    
-    void doSell(int[][] users){
-        int cnt = 0; int total = 0;
-        for(int i=0; i<users.length; i++){
-            int ud = users[i][0]; int up = users[i][1];
-            int price = 0;
-            for(int j=0; j<emoticonList.length; j++){
-                int ed = emoticonList[j].d; int ep = emoticonList[j].p;
-                if(ed>=ud){
-                    price+=(ep/100*(100-ed));
+    void dfs(int len, double[] discountList, int[][] users, int[] emoticons){
+        if(len==emoticons.length){
+            int userCnt = 0;
+            int userTotal = 0;
+            for(int i=0; i<users.length; i++){
+                int account = 0;
+                for(int j=0; j<emoticons.length; j++){
+                    if(users[i][0]<=discountList[j]){
+                        account+=(emoticons[j]*((100-discountList[j])/100));
+                    }
+                }
+                if(users[i][1]<=account){
+                    userCnt++;
+                }
+                else{
+                    userTotal+=account;
                 }
             }
-            if(price>=up){
-                cnt++;
-            }
-            else{
-                total+=price;
-            }
+            cntMax = Math.max(cntMax,userCnt);
+            cntPq.offer(new TotalEmoticon(userCnt,userTotal));
+            //System.out.println(userCnt+" "+usertotal);
+            return;
         }
-        sellQ.add(new Sell(cnt,total));
+        
+        for(int i=0; i<4; i++){
+            discountList[len] = discounts[i];
+            dfs(len+1,discountList,users,emoticons);
+        }
     }
 }
