01536a58180e4ad05df5b90acb49799e8190d245
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-13 15:01:09 +0900

    [level 3] Title: 베스트앨범, Time: 1.63 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,101 +1,82 @@
 import java.util.*;
 
 class Solution {
-    class Genre implements Comparable<Genre>{
-        int genre;
+    class GenreSum{
+        String name;
         int sum;
-        
-        public Genre(int genre, int sum){
-            this.genre = genre;
+        public GenreSum(String name, int sum){
+            this.name = name;
             this.sum = sum;
         }
-        
-        void addSum(int s){
-            this.sum+=s;
-        }
-        
-        @Override
-        public int compareTo(Genre g){
-            return Integer.compare(g.sum,this.sum);
-        }
     }
     
-    class Play implements Comparable<Play>{
-        int i;
-        int p;
-        
-        public Play(int i, int p){
-            this.p = p;
-            this.i = i;
-        }
-        
-        @Override
-        public int compareTo(Play p){
-            if(p.p!=this.p){
-                return Integer.compare(p.p,this.p);
-            }
-            return Integer.compare(this.i,p.i);
+    class Genre{
+        int n;
+        int genre;
+        int play;
+        public Genre(int n, int genre, int play){
+            this.n = n;
+            this.genre = genre;
+            this.play = play;
         }
     }
-    
     
     HashMap<String,Integer> genreMap = new HashMap<>();
+    HashMap<String,Integer> visited = new HashMap<>();
+    int[] genreCnt = new int[100];
     
-    int[] topGenre;
+    List<GenreSum> genreList = new ArrayList<>();
+    
+    PriorityQueue<Genre> pq = new PriorityQueue<>((a,b)->{
+        if(a.genre==b.genre){
+            if(a.play==b.play){
+                return a.n-b.n;
+            }
+            return b.play-a.play;
+        }
+        return a.genre-b.genre;
+    });
     
     public List<Integer> solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        
-        initMap(genres);
-        selectGenre(genres,plays);
-        
-        PriorityQueue<Play> playQ = new PriorityQueue<>();
-        
-        for(int i=0; i<topGenre.length; i++){
-            playQ.clear();
-            int topG = topGenre[i];
-            for(int j=0; j<plays.length; j++){
-                if(genreMap.get(genres[j]) == topG){
-                    playQ.add(new Play(j,plays[j]));
-                }
-            }
-            for(int j=0; j<2; j++){
-                if(playQ.isEmpty()){
-                    break;
-                }
-                answer.add(playQ.poll().i);
-            }
-        }
-        return answer;
-    }
-    
-    void selectGenre(String[] genres, int[] plays){
-        Genre[] genrePlay = new Genre[genreMap.size()];
-
-        for(int i=0; i<genrePlay.length; i++){
-            genrePlay[i] = new Genre(i,0);
-        }
-    
-        for(int i=0; i<plays.length; i++){
-            int g = genreMap.get(genres[i]); int p = plays[i];
-            genrePlay[g].addSum(p);
-        }
-        
-        Arrays.sort(genrePlay);
-        
-        for(int i=0; i<genreMap.size(); i++){
-            topGenre[i] = genrePlay[i].genre;
-        }
-    }
-    
-    void initMap(String[] genres){
-        int genreIndex = 0;
+        int visitedIndex = 0;
         for(int i=0; i<genres.length; i++){
             String g = genres[i];
-            if(!genreMap.containsKey(g)){
-                genreMap.put(g,genreIndex++);
+            if(visited.containsKey(g)){
+                genreList.get(visited.get(g)).sum+=plays[i];
+            }
+            else{
+                genreList.add(new GenreSum(g,plays[i]));
+                visited.put(g,visitedIndex++);
             }
         }
-        topGenre = new int[genreMap.size()];
+        
+        Collections.sort(genreList,(a,b)->{
+            return b.sum-a.sum;
+        });
+        
+        // 재생횟수가 가장 높은 장르가 가장 낮은 index를 가짐 -> 우선순위 큐에서 정렬
+        for(int i=0; i<genreList.size(); i++){
+            genreMap.put(genreList.get(i).name,i);
+        }
+        
+        for(int i=0; i<genres.length; i++){
+            int g = genreMap.get(genres[i]); int p = plays[i];
+            pq.offer(new Genre(i,g,p));
+        }
+        
+        while(!pq.isEmpty()){
+            Genre g = pq.poll();
+            if(genreCnt[g.genre]<2){
+                answer.add(g.n);
+                genreCnt[g.genre]++;
+            }
+        }
+        
+        return answer;
     }
 }
+
+// 장르 고유 번호 필요 -> map 사용
+// 고유 번호 사용하되, plays 횟수가 가장 많은것부터 등록
+// 
