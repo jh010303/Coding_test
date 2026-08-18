4bda6a9dd98e8f37a501af4aaf956281f0ec6734
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-18 20:26:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.56 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -13,13 +13,13 @@ class Solution {
         }
     }
     
-    PriorityQueue<Process> pq1 = new PriorityQueue<>((a,b)->{
-        return a.t - b.t;
-    });
-    PriorityQueue<Process> pq2 = new PriorityQueue<>((a,b)->{
-        return a.r - b.r;
-    });
-    PriorityQueue<Process> pq3 = new PriorityQueue<>((a,b)->{
+    PriorityQueue<Process> pq = new PriorityQueue<>((a,b)->{
+        if(a.t==b.t){
+            if(a.r==b.r){
+                return a.n - b.n;
+            }
+            return a.r - b.r;
+        }
         return a.t - b.t;
     });
     
@@ -35,59 +35,26 @@ class Solution {
         boolean working = false;
         
         while(true){
-            if(jobIndex>=jobs.length && pq1.isEmpty() && !working){ // 종료 조건
+            if(jobIndex>=jobs.length && pq.isEmpty() && !working){ // 종료 조건
                 break;
+            }
+            
+            while(jobIndex<jobs.length && jobs[jobIndex][0]==time){ // 큐에 넣음, 항상 진행해야 함
+                pq.offer(new Process(jobIndex,time,jobs[jobIndex][1]));
+                jobIndex++;
             }
             
             if(working && time == endTime){ // 현재 작업중인 프로세스 종료
                 sum+=(time-requestTime);
                 working = false;
-                //System.out.println(time+" "+requestTime);
             }
             
-            while(jobIndex<jobs.length && jobs[jobIndex][0]==time){ // 큐에 넣음, 항상 진행해야 함
-                pq1.offer(new Process(jobIndex,time,jobs[jobIndex][1]));
-                jobIndex++;
-            }
-            
-            if(!working && !pq1.isEmpty()){ // 작업중인 프로세스 없다면 큐에서 뽑아야 함
-                // pq1에서 뽑음
-                int minT = 1001; int minR = 1001; int minN = 1001;
-                
-                while(!pq1.isEmpty()){ // pq1 -> pq2 작업
-                    Process p1 = pq1.poll();
-                    minT = Math.min(minT,p1.t);
-                    if(p1.t>minT){
-                        pq1.offer(p1);
-                        break;
-                    }
-                    pq2.offer(p1);
-                }
-                
-                while(!pq2.isEmpty()){ // pq2 -> pq3 작업
-                    Process p2 = pq2.poll();
-                    minR = Math.min(minR,p2.r);
-                    if(p2.r>minR){
-                        pq2.offer(p2);
-                        break;  
-                    }
-                    pq3.offer(p2);
-                }
-                
-                Process nextProcess = pq3.poll();
+            if(!working && !pq.isEmpty()){ // 작업중인 프로세스 없다면 큐에서 뽑아야 함
+                Process nextProcess = pq.poll();
                 requestTime = nextProcess.r;
                 endTime = time+nextProcess.t;
                 
-                while(!pq2.isEmpty()){
-                    pq1.offer(pq2.poll());
-                }
-                
-                while(!pq3.isEmpty()){
-                    pq1.offer(pq3.poll());
-                }
-                
                 working = true;
-                //System.out.println("queue pop: "+pq1.size());
             }
             
             time++;
