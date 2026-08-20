614ab04cef1ea441f4ea2746e88a6fdd6c57c200
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-20 13:49:08 +0900

    [level 3] Title: 징검다리 건너기, Time: 21.99 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -3,42 +3,42 @@ import java.util.*;
 class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
-        int l = Integer.MAX_VALUE; int r = Integer.MIN_VALUE; int mid = 0;
-        for(int i=0; i<stones.length; i++){
-            int stone = stones[i];
-            r = Math.max(r,stone);
-            l = Math.min(l,stone);
-        }
+        int l = 1; int r = 200000000;
         
         while(l<=r){
-            mid = (l+r)/2;
-            int maxL = continueZero(stones, mid);
+            int mid = (l+r)/2;
             
-            if(maxL<k){
+            if(checkM(stones,k,mid)){
                 answer = Math.max(answer,mid);
                 l = mid+1;
             }
             else{
                 r = mid-1;
             }
+            
         }
-        
         return answer;
     }
     
-    int continueZero(int[] stones, int mid){
-        int maxCnt = -1;
-        int cont = 0;
+    boolean checkM(int[] stones, int k, int mid){
+        int contin = 0;
         for(int i=0; i<stones.length; i++){
-            if(stones[i]>=mid){
-                cont = 0;
+            if(stones[i]-mid<0){
+                contin++;
             }
             else{
-                cont++;
-                maxCnt = Math.max(maxCnt,cont);
+                contin = 0;
+            }
+ 
+            if(contin>=k){
+                return false;
             }
         }
-        return maxCnt;
+        return true;
     }
 }
 
+// 0 1 2 0 0 0 1 0 2 0
+// stones 원소에서 result만큼 빼고 연속적 음수의 개수가 k미만이면 정답 갱신하고
+// l = mid+1
+// 음수의 연속적인 개수가 k이상이면 r = mid-1
