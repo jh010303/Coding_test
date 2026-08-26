edc5f18073dcb48137f4a206c71a44c1cd5a1689
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-26 17:15:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 108 ms, Memory: 27,932 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,48 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Solution {
-	static int t,n,max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-	static int[] numList;
-	static int[] opCnt = { 0, 0, 0, 0 };
+class Solution
+{
+	static int[] opCnt = new int[4];
 	static char[] op = {'+','-','*','/'};
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		t = Integer.parseInt(br.readLine());
-		
-		for (int test_case = 1; test_case <= t; test_case++) {
-			init();
+	static int[] nums;
+	static int n,maxA,minA;
+	
+	public static void main(String args[]) throws Exception
+	{
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+		int T = Integer.parseInt(br.readLine());
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
 			n = Integer.parseInt(br.readLine());
-			numList = new int[n];
-
+			nums = new int[n];
+			
+			maxA = -100000000; minA = 100000000;
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 4; i++) {
+			for(int i=0; i<4; i++) {
 				opCnt[i] = Integer.parseInt(st.nextToken());
 			}
-
+			
 			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < n; i++) {
-				numList[i] = Integer.parseInt(st.nextToken());
+			for(int i=0; i<n; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
 			}
 			
+			dfs(nums[0],0);
 			
-			backTracking(numList[0],0);
-			sb.append("#").append(test_case).append(" ").append(max-min).append("\n");
+			sb.append("#").append(test_case).append(" ").append(maxA-minA).append('\n');
 		}
-		System.out.println(sb);
 		
+		System.out.print(sb);
 	}
 	
-	static void backTracking(int sum, int depth) {
+	static void dfs(int cur, int depth) {
 		if(depth==n-1) {
-			max = Math.max(sum, max);
-			min = Math.min(sum, min);
+			maxA = Math.max(maxA,cur);
+			minA = Math.min(minA,cur);
 			return;
 		}
 		
@@ -50,26 +51,16 @@ public class Solution {
 			if(opCnt[i]>0) {
 				opCnt[i]--;
 				if(op[i]=='+') {
-					backTracking(sum+numList[depth+1], depth+1);
-				}
-				else if(op[i]=='-') {
-					backTracking(sum-numList[depth+1], depth+1);
-				}
-				else if(op[i]=='*') {
-					backTracking(sum*numList[depth+1], depth+1);
-				}
-				else if(op[i]=='/') {
-					backTracking(sum/numList[depth+1], depth+1);
+					dfs(cur+nums[depth+1],depth+1);
+				}else if(op[i]=='-') {
+					dfs(cur-nums[depth+1],depth+1);
+				}else if(op[i]=='*') {
+					dfs(cur*nums[depth+1],depth+1);
+				}else if(op[i]=='/') {
+					dfs(cur/nums[depth+1],depth+1);
 				}
 				opCnt[i]++;
 			}
 		}
-	}
-	
-	
-	static void init() {
-		Arrays.fill(opCnt, 0);
-		max = Integer.MIN_VALUE;
-		min = Integer.MAX_VALUE;
 	}
 }
