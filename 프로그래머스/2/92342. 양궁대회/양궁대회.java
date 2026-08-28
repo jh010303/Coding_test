d68bc9404fa483738bbdaf67d99fc06df311de2f
JUNE <63304204+jh010303@users.noreply.github.com>
2026-08-28 11:01:21 +0900

    [level 2] Title: 양궁대회, Time: 0.47 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -2,62 +2,59 @@ import java.util.*;
 
 class Solution {
     int[] answer = new int[11];
-    int diff = 0;
-    
+    int diffMax = 1;
     public int[] solution(int n, int[] info) {
-        int[] falseResult = {-1};
-        int[] result = new int[11];
-
-        int apach = 0;
+        int[] ryanList = new int[11];
+        int[] empty = {-1};
+        int apeach = 0;
         for(int i=0; i<info.length; i++){
             if(info[i]>0){
-                apach+=(10-i);
+                apeach+=(10-i);
             }
         }
-        
-        backTracking(10,n,0,apach,result,n,info);
-        
-        boolean flag = false;
-        for(int i=0; i<answer.length; i++){
-            if(answer[i]!=0){
-                flag = true;
-                break;
-            }
+        recurSive(10,n,0,apeach,ryanList,info);
+        if(diffMax==1){
+            return empty;
         }
-        
-        if(flag){
-            return answer;
-        }
-        return falseResult;
+        return answer;
     }
     
-    void backTracking(int cnt, int left, int ryan, int apach, int[] result, int n, int[] info){
-        if(cnt<0){ 
-            if(ryan > apach && ryan-apach>=diff){
-                for(int i=0; i<result.length; i++){
-                    answer[i] = result[i];
-                }
-                if(left>0){
-                    answer[10]+=left;
-                }
-                diff = ryan-apach;
+    void recurSive(int depth, int n, int ryan, int apeach, int[] ryanList, int[] info){
+        if(n<=0 || depth==-1){ // 쏠 화살이 없음
+            if(depth==-1){
+                ryanList[10]+=n;
             }
+            int diff = ryan-apeach;
+            if(diff>diffMax){
+                for(int i=0; i<11; i++){
+                    answer[i] = ryanList[i];
+                }
+                diffMax = diff;
+            }
+            if(depth==-1){
+                ryanList[10]-=n;
+            }
+            //System.out.println(ryan+" "+apeach+" "+Arrays.toString(ryanList));
             return;
         }
-
-        backTracking(cnt-1,left,ryan,apach,result,n,info);
         
-        if(left>=info[cnt]+1){
-            result[cnt] = info[cnt]+1;
-            left-=result[cnt];
-            ryan+=(10-cnt);
-            if(info[cnt]>0){
-                apach-=(10-cnt);
-            }
-            
-            backTracking(cnt-1,left,ryan,apach,result,n,info);
-            
-            result[cnt] = 0;
+        // 라이언이 맞추면서 이기는 선택
+        if(info[depth]<n){
+            ryanList[depth] = info[depth]+1;
+            recurSive(depth-1,n-(info[depth]+1), ryan+10-depth, info[depth]>0?apeach-(10-depth):apeach, ryanList, info);
+            ryanList[depth] = 0;
         }
+        
+        // 라이언이 맞추지 않는 선택 
+        if(info[depth]==0){
+            recurSive(depth-1,n,ryan,apeach,ryanList, info);
+        }
+        else{
+            recurSive(depth-1,n,ryan,apeach, ryanList, info);
+        }
+        
     }
 }
+
+// 각 상태공간(획득할 점수)에서 상대보다 잘 쏠거냐, 포기할거냐 정할 수 있음
+// 이길 수 있는데 지는 선택을 할 수도 있음
