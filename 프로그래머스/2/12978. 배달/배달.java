d6d50bccb70e3818082ab8e31296552b1d8a0b44
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-05 21:26:19 +0900

    [level 2] Title: 배달, Time: 3.52 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,62 +1,62 @@
 import java.util.*;
 
 class Solution {
-    class V{
+    class E implements Comparable<E>{
         int n;
-        int c;
+        int w;
         
-        public V(int n, int c){
+        public E(int n, int w){
             this.n = n;
-            this.c = c;
+            this.w = w;
         }
-
+        
+        @Override
+        public int compareTo(E e){
+            return Integer.compare(this.w,e.w);
+        }
     }
     
-    PriorityQueue<V> pq = new PriorityQueue<>((a,b)->{
-        return Integer.compare(a.c,b.c);
-    });
-    
-    List<List<V>> graph = new ArrayList<>();
     int[] st;
+    List<List<E>> graph = new ArrayList<>();
+    PriorityQueue<E> pq = new PriorityQueue<>();
         
     public int solution(int N, int[][] road, int K) {
-        initGraph(N,road);
         int answer = 0;
         
         st = new int[N+1];
-        Arrays.fill(st,Integer.MAX_VALUE);
-        pq.clear();
-        dijk();
-
-        for(int i=1; i<st.length; i++){
+        Arrays.fill(st,1000000001);
+        
+        initGraph(N,road);
+        
+        pq.offer(new E(1,0));
+        
+        while(!pq.isEmpty()){
+            E cur = pq.poll();
+            int cn = cur.n, cw = cur.w;
+            
+            if(st[cn]<cw){
+                continue;
+            }
+            
+            for(int i=0; i<graph.get(cn).size(); i++){
+                E next = graph.get(cn).get(i);
+                int nn = next.n, nw = next.w+cw;
+                if(st[nn]<nw){
+                    continue;
+                }
+                st[nn] = nw;
+                pq.offer(new E(nn,nw));
+            }
+        }
+        
+        System.out.println(Arrays.toString(st));
+        for(int i=2; i<st.length; i++){
             if(st[i]<=K){
                 answer++;
             }
         }
-        return answer;
-    }
-    
-    void dijk(){
-        pq.offer(new V(1, 0));
-        st[1] = 0;
         
-        while(!pq.isEmpty()){
-            V cur = pq.poll();
-            int cn = cur.n; int cc = cur.c;
-            if(st[cn]<cc){
-                continue;
-            }
-            for(int i=0; i<graph.get(cn).size(); i++){
-                V next = graph.get(cn).get(i);
-                int nn = next.n; int nc = cc+next.c;
-                if(st[nn]<nc){
-                    continue;
-                }
-                pq.offer(new V(nn,nc));
-                st[nn] = nc;
-            }
-        }
-        
+        return answer+1;
     }
     
     void initGraph(int N, int[][] road){
@@ -65,11 +65,9 @@ class Solution {
         }
         
         for(int i=0; i<road.length; i++){
-            int s = road[i][0];
-            int e = road[i][1];
-            int c = road[i][2];
-            graph.get(s).add(new V(e,c));
-            graph.get(e).add(new V(s,c));
+            int s = road[i][0]; int e = road[i][1]; int w = road[i][2];
+            graph.get(s).add(new E(e,w));
+            graph.get(e).add(new E(s,w));
         }
     }
 }
