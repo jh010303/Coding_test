cb8aecb2ba403d0de4a93b575d6313eb1cbd5b39
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-06 21:42:58 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,002 ms, Memory: 129,076 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,88 +1,71 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Solution {
-	static class Edge implements Comparable<Edge> {
-		int s;
-		int n;
-		int w;
-
-		public Edge(int s, int n, int w) {
-			this.s = s;
-			this.n = n;
-			this.w = w;
-		}
-
-		@Override
-		public int compareTo(Edge e) {
-			return this.w - e.w;
-		}
-	}
-
-	static int v, e;
-	static long ans;
-	static Edge[] tree;
+class Solution
+{
 	static int[] parent;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int t = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int test_case = 1; test_case <= t; test_case++) {
+	static int[][] edge;
+	static int count;
+	
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
 			st = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
-			tree = new Edge[e];
-			parent = new int[v + 1];
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			parent = new int[n+1];
+			edge = new int[m][3];
+			count = 0;
 			Arrays.fill(parent, -1);
-			ans = 0;
+			long answer = 0;
 			
-			for (int i = 0; i < e; i++) {
+			for(int i=0; i<m; i++) {
 				st = new StringTokenizer(br.readLine());
-				int s = Integer.parseInt(st.nextToken());
-				int n = Integer.parseInt(st.nextToken());
-				int w = Integer.parseInt(st.nextToken());
-				tree[i] = new Edge(s, n, w);
-			}
-
-			Arrays.sort(tree);
-
-			for (int i = 0; i < e; i++) {
-				Edge cur = tree[i];
-				int s = cur.s;
-				int n = cur.n;
-				int w = cur.w;
-				if (find(s) != find(n)) {
-					union(s, n);
-					ans += w;
-				}
+				int n1 = Integer.parseInt(st.nextToken());
+				int n2 = Integer.parseInt(st.nextToken());
+				int v = Integer.parseInt(st.nextToken());
+				edge[i][0] = n1; edge[i][1] = n2; edge[i][2] = v;
 			}
 			
-			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
+			Arrays.sort(edge,(a,b)->{
+				return Integer.compare(a[2], b[2]);
+			});
+			
+			for(int j=0; j<edge.length; j++) {
+				if(count==n-1) {
+					break;
+				}
+				int n1 = edge[j][0], n2 = edge[j][1], v = edge[j][2];
+				answer+=union(n1,n2,v);
+			}
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
 		}
-
+		
 		System.out.print(sb);
 	}
-
+	
 	static int find(int n) {
-		if (parent[n] < 0)
+		if(parent[n]<0){
 			return n;
-		parent[n] = find(parent[n]);
-		return parent[n];
-	}
-
-	static void union(int a, int b) {
-		int parentA = find(a);
-		int parentB = find(b);
-		if (parentA == parentB)
-			return;
-		else if (parentA < parentB) {
-			parent[parentA] += parent[parentB];
-			parent[parentB] = parentA;
-		} else {
-			parent[parentB] += parent[parentA];
-			parent[parentA] = parentB;
 		}
+		return parent[n] = find(parent[n]);
+	}
+	
+	static int union(int n1, int n2, int v) {
+		int p1 = find(n1), p2 = find(n2);
+		if(p1==p2) {
+			return 0;
+		}
+		parent[p1]+=parent[p2];
+		parent[p2]=p1;
+		count++;
+		return v;
+		
 	}
 }
