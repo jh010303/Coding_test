6e61e0266f2953ad80a6df68a6d2b629ec0f61d7
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-06 21:20:40 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 126 ms, Memory: 28,032 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,49 +1,56 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Solution {
-	static int n,m;
+class Solution
+{
 	static int[] parent;
-	static Set<Integer> set = new HashSet<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        for(int test_case=1; test_case<=t; test_case++){
-            st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-            parent = new int[n+1];
-            set.clear();
-            for(int i=1; i<=n; i++) {
-                parent[i] = i;
-            }
-            for(int i=0; i<m; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                union(a,b);
-            }
-            for(int i=1; i<=n; i++) {
-                set.add(find(i));
-            }
-            
-            sb.append("#").append(test_case).append(" ").append(set.size()).append("\n");
-        }
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			parent = new int[n+1];
+			Arrays.fill(parent, -1);
+			int answer = 0;
+			
+			for(int i=0; i<m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int n1 = Integer.parseInt(st.nextToken());
+				int n2 = Integer.parseInt(st.nextToken());
+				union(n1,n2);
+			}
+			
+			for(int i=1; i<parent.length; i++) {
+				if(parent[i]<0) {
+					answer++;
+				}
+			}
+            sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+		}
+		
 		System.out.print(sb);
 	}
 	
-	static void union(int a, int b) {
-		int parentA = find(a); int parentB = find(b);
-		if(parentA==parentB)return;
-		parent[parentB]=parentA;
+	static int find(int n) {
+		if(parent[n]<0){
+			return n;
+		}
+		return parent[n] = find(parent[n]);
 	}
 	
-	static int find(int n) {
-		if(parent[n]==n)return n;
-		parent[n] = find(parent[n]);
-		return parent[n];
+	static void union(int n1, int n2) {
+		int p1 = find(n1), p2 = find(n2);
+		if(p1==p2) {
+			return;
+		}
+		parent[p1]+=parent[p2];
+		parent[p2]=p1;
 	}
 }
