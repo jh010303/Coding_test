bd704f56cf0f48ec7cfc2cfe0849f13f58863813
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-12 00:14:53 +0900

    [level 1] Title: 시저 암호, Time: 1.69 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder answer = new StringBuilder();
-        
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            if('a'<= c && c<='z'){
-                c = (char)('a'+(c-'a'+n)%26);
-                
-            }else if('A'<=c && c<='Z'){
-                c = (char)('A'+(c-'A'+n)%26);
-    
+        String answer = "";
+        StringBuilder sb = new StringBuilder(s);
+        for(int i=0; i<sb.length(); i++){
+            char c = sb.charAt(i);
+            if(Character.isUpperCase(c)){
+                c=(char)('A'+(c-'A'+n)%26);  
             }
-            answer.append(c);
+            else if(Character.isLowerCase(c)){
+                c=(char)('a'+(c-'a'+n)%26);    
+            }
+            sb.setCharAt(i,c);
         }
-        return answer.toString();
+        return sb.toString();
     }
 }
-// 문자가 대문자인지, 소문자인지 판단하는 함수?
-// 문자를 대문자, 소문자로 바꾸는 함수
