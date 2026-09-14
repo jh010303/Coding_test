b1598c3fb6b4c9b9e2dcbac9671dc2f17a9b6583
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-14 20:10:20 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 107 ms, Memory: 30,336 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,49 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Solution {
-	static int t,n,m,ans;
-	static int[] snacks;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuffer sb = new StringBuffer();
-		t = Integer.parseInt(br.readLine());
-		for (int test_case = 1; test_case <= t; test_case++) {
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+        
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
 			st = new StringTokenizer(br.readLine());
-			ans=-1;
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken());
-			snacks = new int[n];
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			int[] arr = new int[n];
+			int answer = 0;
+			
 			st = new StringTokenizer(br.readLine());
 			for(int i=0; i<n; i++) {
-				snacks[i] = Integer.parseInt(st.nextToken());
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
-			Arrays.sort(snacks);
-			int start = 0, end = n-1;
-			while(start<end) {
-				int weight = snacks[start]+snacks[end];
-				if(weight==m) {
-					ans = weight;
+			
+			Arrays.sort(arr);
+			int l=0; int r = n-1;
+			while(l<r) {
+				int sum = arr[l]+arr[r];
+				if(sum==m) {
+					answer = sum;
 					break;
-				}else if(weight>m) {
-					end--;
-				}else {
-					ans = Math.max(ans, weight);
-					start++;
+				}
+				else if(sum>m) {
+					r--;
+				}
+				else {
+					answer = Math.max(answer, sum);
+					l++;
 				}
 			}
-			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
+			
+            sb.append("#").append(test_case).append(" ").append(answer==0?-1:answer).append("\n");
 		}
-
-		System.out.println(sb);
+		
+		System.out.print(sb);
 	}
 }
