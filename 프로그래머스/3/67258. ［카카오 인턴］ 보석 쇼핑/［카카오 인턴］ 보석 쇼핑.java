f2abdf797cb3dbb80c476728910aabb446b7504d
JUNE <63304204+jh010303@users.noreply.github.com>
2026-09-15 19:18:22 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 28.25 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,51 +1,55 @@
 import java.util.*;
 
 class Solution {
-    HashMap<String,Integer> gemindex = new HashMap<>();
-    int[] visited;
-    int minL = Integer.MAX_VALUE;
+    HashMap<String,Integer> mp = new HashMap<>();    
+    HashSet<String> st = new HashSet<>();
+    
     public int[] solution(String[] gems) {
-        initGemIndex(gems);
         int[] answer = new int[2];
-        visited = new int[gemindex.size()];
-        int l=0; int r=0;
-        while(true){
-            if(checkVisited()){
-                String gem = gems[l++];
-                visited[gemindex.get(gem)]--;
-                if(r-l<minL){
-                    minL = r-l;
-                    answer[0] = l;
-                    answer[1] = r;
-                }
+        for(int i=0; i<gems.length; i++){
+            st.add(gems[i]);
+        }
+        
+        int p1=0; int p2=0; int mSize = 100001;
+        mp.put(gems[p1],mp.getOrDefault(gems[p1],0)+1);
+        
+        while(p1<=p2){
+            if(mSize==0){
+                break;
             }
-            else{
-                if(r>=gems.length){
+            
+            boolean flag = true;
+            if(st.size()!=mp.size()){
+                flag = false;
+            }
+            
+            if(!flag){ // 조건 만족 못하니 증가시켜야 함
+                if(p2<gems.length-1){
+                    p2++;
+                    mp.put(gems[p2],mp.getOrDefault(gems[p2],0)+1);
+                } 
+                else{ // 증가 시키려고 하는데 마지막이면 더 볼필요 없음
                     break;
                 }
-                String gem = gems[r++];
-                visited[gemindex.get(gem)]++;
-            } 
+            }
+            else{ // 조건 만족 하니 감소시켜야 함
+                if(p2-p1<mSize){
+                    mSize = p2-p1;
+                    answer[0] = p1+1; answer[1] = p2+1;
+                }
+                mp.put(gems[p1],mp.get(gems[p1])-1);
+                if(mp.get(gems[p1])==0){
+                    mp.remove(gems[p1]);
+                }
+                p1++;
+            }
         }
+        
         return answer;
     }
-    
-    boolean checkVisited(){
-        for(int i=0; i<visited.length; i++){
-            if(visited[i]<=0){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    void initGemIndex(String[] gems){
-        int index = 0;
-        for(int i=0; i<gems.length; i++){
-            String gem = gems[i];
-            if(!gemindex.containsKey(gem)){
-                gemindex.put(gem,index++);
-            }
-        }
-    }
 }
+
+// map에 보석들 저장
+// gems 배열돌면서 모든 보석들의 v가 1이상이 될 때 까지 p2 증가
+// 만약 1이상이면 지금 윈도우 길이 정답 갱신하고 ( 작다면 ) p1 증가
+// p2가 배열 길이 벗어나면 중지
